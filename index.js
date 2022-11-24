28f397b3b1aecfc07fb47e2c07aaf1320555a55b
otienoonyango2000 <otienoonyango2000@gmail.com>
2022-11-24 14:41:38 +0300

    Arithmetic-lab assignmet completed

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num2 * num1;
+console.log(multiply);
+
+//random
+let random = 0;
+++random
+console.log(random);
+
+//module
+const num3 = 5;
+const num4 = 9;
+const mod = num4 % num3;
+console.log(mod);
+
+//max
+const max = Math.max(1, 2, 3, 20, 5);
+console.log(max)
+
